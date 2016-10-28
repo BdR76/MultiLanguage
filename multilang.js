5b3ae2aedc69ca4f9a3a0315fef4c42ad3c3d2b7
BdR76 <bdr1976@gmail.com>
2016-10-28 18:08:16 +0200

    JavaScript multi-language localisation
    Multi-language localisation example in JavaScript, using a json file for
    translation texts and with a working example page.

---
+++ multilang.js
@@ -0,0 +1,74 @@
+// MultiLang - BdR 2016
+// JavaScript object to handle multilanguage, load phrases from JSON etc.
+
+var MultiLang = function(url, lang, onload)
+{
+	// variables
+	this.phrases = {};
+
+	// keep only first two chareacters, for example 'en-US', 'fr', 'nl-NL', 'it', 'zh' etc.
+	this.selectedLanguage = (lang || navigator.language || navigator.userLanguage).substring(0, 2);;
+	
+	// onLoad callback function, call after loading JSON
+	this.onLoad = onload;
+
+	// load json from url
+	if (typeof url !== 'undefined') {
+		var obj = this;
+		var req = new XMLHttpRequest();
+
+		// NOTE: will load asynchronously!
+		req.open("GET", url, true);
+		//req.setRequestHeader("User-Agent",navigator.userAgent);
+		req.onreadystatechange = function (evt) {
+			if (evt.target.readyState == 4 && evt.target.status == 200)
+			//if (evt.target.readyState == 4)// TESTING loading from local file (status == 0)
+			{
+				// load translations
+				this.phrases = JSON.parse( evt.target.responseText );
+
+				// verify that the currently selected language exists in the translations
+				this.setLanguage(this.selectedLanguage);
+				
+				// do callback when loading JSON is ready
+				if (this.onLoad) {
+					this.onLoad();
+				}
+
+			};
+		}.bind(obj); // NOTE: bind onreadyfunction to MultiLang instead of XMLHttpRequest, so MultiLang.phrases will be set instead of added to XMLHttpRequest
+		req.send(null);
+	};
+
+	this.setLanguage = function(langcode) {
+
+		// check if language code exists in translations
+		if (!this.phrases.hasOwnProperty(langcode)) {
+			// if it doesn't exist; default to first language 
+			
+			// NOTE: the order of properties in a JSON object are not *guaranteed* to be the same as loading time,
+			// however in practice all browsers do return them in order
+			for (var key in this.phrases) {
+				if (this.phrases.hasOwnProperty(key)) {
+					langcode = key;
+					break;
+				};
+			};
+		};
+
+		// set as selected language code
+		this.selectedLanguage = langcode;
+	};
+
+	this.get = function(key) {
+		// get key phrase
+		var str = this.phrases[this.selectedLanguage][key];
+
+		// if key does not exist, return the literal key
+		str = (str || key);
+
+		return str;
+	};
+}
+
+
---
+++ readme.md
@@ -0,0 +1,37 @@
+MultiLang.js
+============
+
+MultiLang.js is a JavaScript object to handle multilanguage, localisation, JSON file loading, detect default language, detect non-existing phrases etc.
+
+It contains a working example page and a JSON example file with multiple languages. Initially created for Phaser.js games, but it can be applied to any JavaScript project or website.
+
+How to use
+----------
+All translations are stored in a JSON file like this:
+
+	{
+	   "en": {
+	      "Hello": "Hello World!",
+	      "Yes": "Yes"
+	   },
+	   "fr": {
+	      "Hello": "Bonjour à tous!",
+	      "Yes": "Oui"
+	   }
+	}
+
+And then MultiLang.js can be used like this:
+
+	var multilang = new MultiLang('languages.json');
+	multilang.setLanguage('fr');
+	alert( multilang.get('Hello') );
+
+Manage translations
+-------------------
+Keeping track of all the translated strings can be a challenge, even for smaller projects. See this [Excel spreadsheet](http://members.home.nl/bas.de.reuver/files/multilanguage.zip) to manage translations more easily. The translator or translators can fill in the spreadsheet with translated texts and the JSON file can then be generated using VBA. For more information [see here](http://stackoverflow.com/questions/20324967/localize-multi-plattform-projects-consolidate-string-files).
+
+History
+-------
+2016-okt-28 upload project on github
+
+Send any questions or comments to Bas de Reuver - bdr1976@gmail.com
